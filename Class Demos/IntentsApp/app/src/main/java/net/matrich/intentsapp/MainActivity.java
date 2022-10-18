c7bc06ec4bdda12bad73f6e303273d0059bbf6b9
Richard Matovu <rkmatrich@gmail.com>
2022-10-18 05:36:35 -0400

    intents with result

--- Class Demos/IntentsApp/app/src/main/java/net/matrich/intentsapp/MainActivity.java
+++ Class Demos/IntentsApp/app/src/main/java/net/matrich/intentsapp/MainActivity.java
@@ -1,5 +1,6 @@
 package net.matrich.intentsapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +16,16 @@ public class MainActivity extends AppCompatActivity {
     EditText mEditTextMessage;
     public static final String EXTRA_MSG = "android.intents.extra.MESSAGE";
 
+    public static final int TEXT_REQUEST = 1;
+    private TextView mTextviewReply;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mEditTextMessage = findViewById(R.id.edittext_message);
+        mTextviewReply = findViewById(R.id.textview_header_reply);
     }
 
     public void launchSecondActivity(View view) {
@@ -28,8 +34,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SecondActivity.class);
         String msg = mEditTextMessage.getText().toString();
         intent.putExtra(EXTRA_MSG, msg);
-        startActivity(intent);
+        //startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mTextviewReply.setVisibility(View.VISIBLE);
+                mTextviewReply.setText(reply);
+            }
+        }
 
     }
 }
--- Class Demos/IntentsApp/app/src/main/java/net/matrich/intentsapp/SecondActivity.java
+++ Class Demos/IntentsApp/app/src/main/java/net/matrich/intentsapp/SecondActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    public static final String EXTRA_REPLY = "twoactivities.extra.REPLY";
+    private EditText mEditTextReply;
     TextView mTextViewMessage;
 
     @Override
@@ -15,10 +19,22 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        mEditTextReply = findViewById(R.id.edittext_message);
+
         Intent intent = getIntent();
         String msg = intent.getStringExtra(MainActivity.EXTRA_MSG);
 
         mTextViewMessage = findViewById(R.id.textview_message);
         mTextViewMessage.setText(msg);
+    }
+
+    public void returnReply(View view) {
+        String reply = mEditTextReply.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+
     }
 }
